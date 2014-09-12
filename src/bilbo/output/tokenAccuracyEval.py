13a81204dffd2f16c0e3ecf0d68d71c733c756a7
Anaïs Ollagnier <ollagnier81@gmail.com>
2014-09-12 14:47:09 +0200

    Évaluation: ajout du calcul de la macro précision
    Conflicts:
    	src/bilbo/output/tokenAccuracyEval.py

--- src/bilbo/output/tokenAccuracyEval.py
+++ src/bilbo/output/tokenAccuracyEval.py
@@ -15,12 +15,16 @@ import sys
 import os
 import string
 
-excluded = {'nonbibl': 0, 'c':0}
+excluded = {'nonbibl': 0, 'c':0} #label exclue sauf pour le calcul de la micro
 
-cnt = {'0000': 0}
-acc = {'0000': 0}
-cnt_d = {'0000': 0}
+cnt = {'0000': 0}# nombre de token étiqueté par label dans le test (ce que l'on souhaite avoir)
+acc = {'0000': 0} # nombre d'étiquette correct par label
+cnt_d = {'0000': 0}# nombre de token étiqueté par label dans la référence (ce qu'il faut avoir)
 errors = {'0000':{'000':0}} #{desired:{predicted:count}}
+tab_precision = []
+tab_recall = []
+tab_mainElmt_precision = []
+tab_mainElmt_recall = []
 
 def evaluate(srcfile, dsrfile):
 	
@@ -39,8 +43,8 @@ def evaluate(srcfile, dsrfile):
 				acc[currstr] = 0
 	del cnt['0000']
 
-	j = 0
-	c = 0
+	j = 0 # nombre total de token étiqueté
+	c = 0 #nombre total d'étiquette correct prédite
 	for line in open (dsrfile, 'r') :
 		line = string.split(line)
 		if len(line) != 0 :
@@ -48,8 +52,10 @@ def evaluate(srcfile, dsrfile):
 			if cnt_d.has_key(currstr) :
 				cnt_d[currstr] += 1
 			else :
-				cnt_d[currstr] = 1			
+				cnt_d[currstr] = 1
+						
 			if (src[j] == currstr) : #predicted==desired
+				
 				c += 1
 				acc[currstr] += 1
 			else :
@@ -64,25 +70,34 @@ def evaluate(srcfile, dsrfile):
 				
 			j += 1
 	del acc['0000']
-	del cnt_d['0000']
-	
+	del cnt_d['0000']	
+
 	print '\nTotal accuracy (Micro Averaged Precision)'
 	print '#', c, j, float(c)/float(j)*100
 	
 	for key in excluded.keys() :
 		if cnt_d.has_key(key) :
 			print '\n-',key
-			apr = float(c - acc[key])/float(j - cnt[key])*100
+			apr = float(c - acc[key])/float(j - cnt[key])*100 #c-acc[key] est le nombre d'étiquette prédite moins le nombre d'étiquette correct prédite pour le label exclu divisé par le nombre d'étiquette moins le nombre d'étiquette exclue
 			are = float(c - acc[key])/float(j - cnt_d[key])*100
-			print '(Averaged Precision)', (c - acc[key]), (j - cnt[key]), apr
-			print '(Averaged Recall)', (c - acc[key]), (j - cnt_d[key]), are
-			print '(F-measure)', 2*apr*are/(apr+are)
+			print '(Averaged micro Precision) exclu ', key, (c - acc[key]), (j - cnt[key]), apr
+			print '(Averaged micro Recall)', key, (c - acc[key]), (j - cnt_d[key]), are
+			print '(micro F-measure) exclu ', key, 2*apr*are/(apr+are)
+	
 		
 	print '\n***** Precision *****'
 	
 	for key, value in sorted(cnt_d.iteritems(), key=lambda (k,v): (v,k), reverse=True):
 		if acc.has_key(key) :
-			print key, acc[key], cnt[key], float(acc[key])/float(cnt[key])*100
+			result_key = float(acc[key])/float(cnt[key])*100
+			tab_precision.append(result_key)
+			print key, acc[key], cnt[key], result_key
+		if key == 'title':
+			tab_mainElmt_precision.append(float(acc[key])/float(cnt[key])*100)
+		if key == 'surname':
+			tab_mainElmt_precision.append(float(acc[key])/float(cnt[key])*100)
+		if key == 'forename':
+			tab_mainElmt_precision.append(float(acc[key])/float(cnt[key])*100)
 		
 	
 	#print '\n***** Precision *****'
@@ -92,9 +107,18 @@ def evaluate(srcfile, dsrfile):
 	print '\n***** Recall *****'
 	for key, value in sorted(cnt_d.iteritems(), key=lambda (k,v): (v,k), reverse=True):
 		if acc.has_key(key) :
-			print key, acc[key], value, float(acc[key])/float(value)*100
+			result_key = float(acc[key])/float(value)*100
+			tab_recall.append(result_key)
+			print key, acc[key], value, result_key
 		else :
 			print key, 0, cnt_d[key], 0.0
+		if key == 'title':
+			tab_mainElmt_recall.append(float(acc[key])/float(value)*100)
+		if key == 'surname':
+			tab_mainElmt_recall.append(float(acc[key])/float(value)*100)
+		if key == 'forename':
+			tab_mainElmt_recall.append(float(acc[key])/float(value)*100)
+		
 	print
 	
 	#print '\n***** Recall *****'
@@ -104,8 +128,24 @@ def evaluate(srcfile, dsrfile):
 	#	else :
 	#		print k, 0, cnt_d[k], 0.0
 	#print
-	
-	
+
+	print '\n***** Macro Precision/Recall *****'
+	macro_precision = sum(tab_precision)/len(tab_precision)
+	macro_rappel = sum(tab_recall)/len(tab_recall)
+
+	macro_precision_mainElmt = sum(tab_mainElmt_precision)/len(tab_mainElmt_precision)
+	macro_recall_mainElmt =	sum(tab_mainElmt_recall)/len(tab_mainElmt_recall)
+
+	print '(macro precision all elements)', macro_precision
+	print '(macro rappel all elements)', macro_rappel
+	print '(macro F-mesure all elements)', (2*(macro_precision*macro_rappel))/(macro_precision+macro_rappel)
+	print '\n'
+	print '(macro precision three elements)', macro_precision_mainElmt
+	print '(macro rappel three elements)', macro_recall_mainElmt
+	print '(macro F-mesure three elements)', (2*(macro_precision_mainElmt*macro_recall_mainElmt))/(macro_precision_mainElmt+macro_recall_mainElmt)
+
+	print '\n'
+
 	tes = 0
 	bes = 0
 	ts = 0
@@ -142,7 +182,6 @@ def main():
 	if len (sys.argv) != 3 :
 		print 'python tokenAccuracyEval.py (file to be evaluated) (correct label file)'
 		sys.exit (1)
-
 	evaluate(str(sys.argv[1]), str(sys.argv[2]))
 
 
