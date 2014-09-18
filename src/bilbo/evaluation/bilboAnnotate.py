f3122726e09daa06ee5e423db57cf3b4095e49af
ArnAud Cordier <arnaud.cordier@openedition.org>
2014-09-18 15:46:18 +0200

    Évaluation: automatisation de l'annotation

---
+++ src/bilbo/evaluation/bilboAnnotate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import sys
+if __name__ == '__main__':
+	sys.path.append('src/')
+import os
+from bilbo.Bilbo import Bilbo
+from bilbo.utils import *
+from partition import Partition
+import glob
+import shutil
+
+'''
+ foreach directory-evaluation/10%/ directory
+ annotate with bilbo with 01/test/test.xml file
+ save result in 01/result/
+'''
+
+class bilboAnnotate():
+	def __init__(self, options, dirCorpus, testPercentage, numberOfPartition = 10):
+		options.L = True
+		options.t = 'bibl'
+		options.k = 'all'
+		print options
+		
+		dirPartitions = Partition.getDirPartitionNames(dirCorpus, testPercentage, numberOfPartition)
+		for dirPartition in dirPartitions:
+			print "dirPartition", dirPartition
+			(testDir, trainDir, modelDir, resultDir) = Partition.getDirTestNames(dirPartition)
+			
+			self._del_tmp_file(resultDir)
+			bilbo = Bilbo(resultDir, options, "crf_model_simple")
+			bilbo.annotate(testDir, modelDir, 1)
+
+	def _del_tmp_file(self, resultDir):
+		pattern = os.path.join(resultDir,'tmp*')
+		tmpDirs = glob.glob(pattern)
+		for tmpDir in tmpDirs:
+			shutil.rmtree(tmpDir)
+
+if __name__ == '__main__':
+	parser = defaultOptions()
+	options, args = parser.parse_args(sys.argv[1:])
+	
+	# usage python src/bilbo/evalution/bilboAnnotate.py [bilbo option] dirCorpus 10
+	p = bilboAnnotate(options, str(sys.argv[1]), str(sys.argv[2]))
+	
