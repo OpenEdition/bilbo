9d794776d4d684781ba00178a28d793eb8fed191
ArnAud Cordier <arnaud.cordier@openedition.org>
2014-09-24 15:01:48 +0200

    Évaluation: fichier .tsv récapitulatif des moyennes de chaque eval

--- eval.sh
+++ eval.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # utilisation : ./eval.sh dirCorpus numberOfpartition prefix percentOfTest [percentOfTest…] -- bilbo options
-# exemple     : ./eval.sh Corpus 10 huhu 10 20 30 40 50
+# exemple     : ./eval.sh Corpus 10 huhu 10 20 30 40 50 -- -u
 # it will
 #  train on Corpus folder
 #  do 10 partitions
 #  in Corpus-eval-huhu folder
 #  doing 10% 20% 30% 40% 50% of test
-
+# with option -u for bilbo
 
 dirCorpus=${1/\//}
 shift
@@ -15,6 +15,7 @@ numberOfpartition=$1
 shift
 prefix=$1
 shift
+dirEval=${dirCorpus}-eval-${prefix}
 
 args="$@"
 percents=${args%%--*}
@@ -34,4 +35,12 @@ for percentOfTest in $percents; do
 	python src/bilbo/evaluation/bilboAnnotate.py $bilboOptions ${dirCorpus} $percentOfTest $numberOfpartition $prefix;
 	echo "  evaluating…"
 	python src/bilbo/evaluation/bilboEval.py ${dirCorpus} $percentOfTest $numberOfpartition $prefix;
+done;
+
+# Création du fichier global de l'évaluation
+globalEvalFile=${dirEval}/evaluation.tsv
+head -n 1 ${dirEval}/${percents%% *}%/evaluation.tsv | sed "s/^/% of test data\t/" > $globalEvalFile
+for percentOfTest in $percents; do
+	tail -n 1 ${dirEval}/${percentOfTest}%/evaluation.tsv | sed "s/^/${percentOfTest}%\t/" >> $globalEvalFile
+	echo >> $globalEvalFile
 done;
